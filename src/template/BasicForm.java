978fa9f5ce37e28aee2b130e7f805fe9d483df2c
Andrew Junzki <sswgmxain@gmail.com>
2014-03-17 23:46:20 +0800

    编写列表模板 完成接口部分 具体实现尚未填充

---
+++ src/template/BasicForm.java
@@ -0,0 +1,51 @@
+package template;
+
+/**
+ * Created by andrew on 14-3-17.
+ */
+public class BasicForm {
+    private int id;
+    private String name;
+    private int grade;
+
+    /** 构造函数：初始化链表 */
+    public BasicForm()
+    {
+
+    }
+
+    /** 增加一个新的项目到列表 */
+    public void add(String nameToAdd,
+                    int gradeToAdd)
+    {
+
+    }
+
+    /** 从列表中删除一个项目 */
+    public void delete(int idToDel)
+    {
+
+    }
+
+    /** 根据id更新对应项的姓名 */
+    public void update(int idToUpdate,
+                       String nameToUpdate)
+    {
+
+    }
+
+    /** 根据id更新对应项的成绩 */
+    public void update(int idToUpdate,
+                       int gradeToUpdate)
+    {
+
+    }
+
+    /** 根据id更新对应项的姓名和成绩 */
+    public void update(int idToUpdate,
+                       String nameToUpdate,
+                       int gradeToUpdate)
+    {
+
+    }
+}
